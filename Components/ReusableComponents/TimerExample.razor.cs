0e0b77586315572f5dd63462fa0f84adf643fc5a
gaplin <gaplinn@gmail.com>
2023-11-15 23:00:00 +0100

    Add project files.

---
+++ Components/ReusableComponents/TimerExample.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace TimerRepro.Components.ReusableComponents;
+
+public sealed partial class TimerExample : IDisposable
+{
+    private int _ticks = 0;
+
+    private readonly Timer _timer = new(1000);
+    private bool _disposed = false;
+
+    private void OnTimedEvent(object? source, ElapsedEventArgs e)
+    {
+        _ticks += 1;
+        InvokeAsync(StateHasChanged);
+    }
+
+    protected override void OnInitialized()
+    {
+        _timer.Elapsed += OnTimedEvent;
+        _timer.Enabled = true;
+        _timer.AutoReset = true;
+        _timer.Start();
+    }
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            _timer.Elapsed -= OnTimedEvent;
+            _timer.Dispose();
+            _disposed = true;
+        }
+        GC.SuppressFinalize(this);
+    }
+}
